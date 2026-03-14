009ef92088d1162c96f9f4b3e770e43fb512d5fa
Dhoop Patel <dhoopspatel757@gmail.com>
2026-03-14 16:11:46 -0700

    Update server URL to production endpoint

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -4,7 +4,7 @@ import { createContext } from 'react';
 export const AuthDataContext = createContext();
 
 const AuthContext = ({ children }) => {
-    const serverUrl = "http://localhost:8000"; // backend server url
+    const serverUrl = "https://connectin-backend-w4o8.onrender.com"; // backend server url
     let value = {serverUrl};
 
     return (
