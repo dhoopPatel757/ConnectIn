718c28148eaed8a401d33e523f5fb3bf4f51d1ed
Dhoop Patel <dhoopspatel757@gmail.com>
2026-03-15 11:43:16 -0700

    Update multer storage to generate unique filenames
    Modified filename generation to include a unique timestamp and random number.

--- Backend/middlewares/multer.js
+++ Backend/middlewares/multer.js
@@ -4,8 +4,13 @@ let storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./public");
   },
+  // filename: (req, file, cb) => {
+  //   cb(null, file.originalname);
+  // }
+
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    const unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, unique + "-" + file.originalname);  // ✅ unique filename
   }
 });
 
