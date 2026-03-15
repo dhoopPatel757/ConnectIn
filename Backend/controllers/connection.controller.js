1d066da780be6d1fe1682754cfb331b5109ae986
Dhoop Patel <dhoopspatel757@gmail.com>
2026-03-15 13:32:56 -0700

    Disable getSuggestedUser function
    Comment out the getSuggestedUser function to disable it.

--- Backend/controllers/connection.controller.js
+++ Backend/controllers/connection.controller.js
@@ -231,22 +231,22 @@ export const getConnectionRequest = async(req,res) => {
     }
 }
 
-export const getSuggestedUser = async(req,res) => {
-    try{ 
-        let currentUser = await User.findById(req.userId).select("connection");
+// export const getSuggestedUser = async(req,res) => {
+//     try{ 
+//         let currentUser = await User.findById(req.userId).select("connection");
         
-        let suggestedUsers = await User.find({
-            _id : {
-                $ne : req.userId,
-                $nin : currentUser.connection
-            }
-        }).select("-password");
-        return res.status(200).json(suggestedUsers);
-    }catch(err){
-        console.log(err);
-        return res.status(500).json({message : "Internal Server Error"});
-    }
-}
+//         let suggestedUsers = await User.find({
+//             _id : {
+//                 $ne : req.userId,
+//                 $nin : currentUser.connection
+//             }
+//         }).select("-password");
+//         return res.status(200).json(suggestedUsers);
+//     }catch(err){
+//         console.log(err);
+//         return res.status(500).json({message : "Internal Server Error"});
+//     }
+// }
 
 
 export const getUserConnections = async(req,res) => {
